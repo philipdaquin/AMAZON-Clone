87be2681d0e3d04b19f627e232752c16c7b4b4a9
philipdaquin <philipdaquin20@gmail.com>
2022-02-16 21:50:02 +0800

    state providers

---
+++ frontend/src/components/state_provider.rs
@@ -0,0 +1,15 @@
+use yew::{prelude::*, function_component, html, Html};
+
+pub struct StateContext { 
+    
+}
+
+
+
+#[function_component(StateProviders)]
+pub fn stateprovider() -> Html {
+    html! {
+        <>
+        </>
+    }
+}
