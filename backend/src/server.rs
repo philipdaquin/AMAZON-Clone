49a7172d3e4c02fcd48cbf1f812770335d0c9b1b
philipdaquin <philipdaquin20@gmail.com>
2022-02-26 08:44:55 +0800

    Update to use stable actix

--- backend/src/server.rs
+++ backend/src/server.rs
@@ -1,4 +1,4 @@
-use crate::handlers::{product};
+use crate::handlers::{product::{self, index}};
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
 
@@ -7,7 +7,7 @@ pub async fn new_server(port: u32) -> std::io::Result<()> {
         //  App Routes
         App::new()
             //  Everything under /product/
-            .service(web::scope("/product/")
+            .service(web::resource("/product/")
                 //  Creates a new product and returns its id    
                 //.route("", web::post().to())
                 //  Returns information about the product with id
@@ -17,7 +17,7 @@ pub async fn new_server(port: u32) -> std::io::Result<()> {
                 //  Marks the product with id as delete
                 //.route("/product/{id}", web::delete().to())
                 //  Returns a list of all products 
-                .route("", web::get().to( product::index))
+                .route(web::get().to(index))
             
         )
 
