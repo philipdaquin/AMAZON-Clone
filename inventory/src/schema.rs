06f2a865e1366a5c8b5eacb508c13d9e88e45782
philipdaquin <philipdaquin20@gmail.com>
2022-03-07 18:24:29 +0800

    schema updarted manually

--- inventory/src/schema.rs
+++ inventory/src/schema.rs
@@ -1,9 +1,13 @@
 table! {
+    use diesel_full_text_search::TsVector;
+    use diesel::sql_types::{Int4, Varchar, Float8, Nullable};
     products (id) {
         id -> Int4,
         title -> Varchar,
         stock -> Float8,
         rating -> Nullable<Float8>,
         price -> Nullable<Int4>,
+        description -> Nullable<Varchar>,
+        text_searchable_product_col -> TsVector,
     }
 }
