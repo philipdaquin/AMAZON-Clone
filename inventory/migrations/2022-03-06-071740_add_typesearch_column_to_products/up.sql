6def10b8b2a07e338eab51b8e854e1fc96a65985
philipdaquin <philipdaquin20@gmail.com>
2022-03-08 19:27:48 +0800

    GIN to RUM

--- inventory/migrations/2022-03-06-071740_add_typesearch_column_to_products/up.sql
+++ inventory/migrations/2022-03-06-071740_add_typesearch_column_to_products/up.sql
@@ -3,7 +3,7 @@ ALTER TABLE products ADD COLUMN text_searchable_product_col tsvector NOT NULL;
 UPDATE products SET text_searchable_product_col = 
     to_tsvector('english', title || ' ' || coalesce(description, ''));
 
-CREATE INDEX textsearch_idx ON products USING GIN (text_searchable_product_col);
+CREATE INDEX textsearch_idx ON products USING RUM (text_searchable_product_col);
 
 CREATE TRIGGER tsvectorupdateproducts BEFORE INSERT OR UPDATE
 ON products FOR EACH ROW EXECUTE PROCEDURE
--- inventory/src/schema.rs
+++ inventory/src/schema.rs
@@ -1,6 +1,4 @@
 table! {
-    use diesel_full_text_search::TsVector;
-    use diesel::sql_types::{Int4, Varchar, Float8, Nullable};
     products (id) {
         id -> Int4,
         title -> Varchar,
@@ -8,6 +6,6 @@ table! {
         rating -> Nullable<Float8>,
         price -> Nullable<Int4>,
         description -> Nullable<Varchar>,
-        text_searchable_product_col -> TsVector,
+        text_searchable_product_col -> Tsvector,
     }
 }
