35a1748111a0102b941bee6a40e9410c0c7dfa7f
philipdaquin <philipdaquin20@gmail.com>
2022-03-15 19:49:52 +0800

    Sales

--- inventory/src/models/product_sales.rs
+++ inventory/src/models/product_sales.rs
@@ -1,7 +1,15 @@
+
+use diesel::{Insertable, AsChangeset};
+use serde::Deserialize;
+
 use crate::models::product::{Product, NewProduct};
+use crate::schema::sale_products;
+use crate::models::sales::Sale;
 
-
-
+#[derive(Identifiable, Associations, Queryable, Debug, Clone, PartialEq)]
+#[table_name = "sale_products"]
+#[belongs_to(Sale)]
+#[belongs_to(Product)]
 pub struct ProductSaleInfo { 
     pub id: i32,
     pub product_id: i32,
@@ -16,16 +24,17 @@ pub struct ProductForSale  {
     pub sale_info: ProductSaleInfo,
     pub product: Product
 }
-
+#[derive(Insertable, Deserialize, Serialize, AsChangeset, Debug, Clone, PartialEq)]
+#[table_name = "sale_products"]
 pub struct NewProductSale { 
     pub id: Option<i32>,
     pub product_id: Option<i32>,
     pub sale_id: Option<i32>, 
-    pub amount: Option<i32>, 
+    pub amount: Option<f64>, 
     pub discount: Option<i32>, 
     pub tax: Option<i32>, 
     pub price: Option<i32>,
-    pub total: Option<i32>,
+    pub total: Option<f64>,
 }
 
 pub struct NewProductSaleInfo  {
--- inventory/src/models/sales.rs
+++ inventory/src/models/sales.rs
@@ -1,39 +1,41 @@
 use diesel::{result::Error as DbError, 
     PgConnection, BelongingToDsl, sql_types,
 };
-use diesel::{RunQueryDsl, QueryDsl};
+use diesel::{RunQueryDsl, QueryDsl, ExpressionMethods};
 use crate::schema::{self, sales, sale_products};
 use crate::types::{ProductColumns, PRODUCT_COLUMNS};
 use juniper::FieldResult;
-use chrono::NaiveTime;
+use chrono::NaiveDate;
+use crate::models::product_sales::NewProductSaleInfo;
 
 
-
-
-
-pub struct Sales { 
+#[derive(Identifiable, Queryable, Debug, Clone, PartialEq)]
+#[table_name = "sales"]
+pub struct Sale { 
     pub id: i32, 
     pub user_id: i32,
-    pub sale_date: NaiveTime,
+    pub sale_date: NaiveDate,
     pub total: f64,
     pub bill_number: Option<String>
 }
 
+#[derive(Insertable, Deserialize, Serialize, AsChangeset, Debug, Clone, PartialEq)]
+#[table_name = "sales"]
 pub struct NewSale { 
     pub id: Option<i32>, 
     pub user_id: Option<i32>,
-    pub sale_date: Option<NaiveTime>,
+    pub sale_date: Option<NaiveDate>,
     pub total: Option<f64>,
-    pub bill_number: Option<String>
 }
 
+#[derive(Debug, Clone)]
 pub struct FullSale { 
-    pub sale: Sales,
+    pub sale: Sale,
     pub sales_products: Vec<FullNewSales>
 }
 
 pub struct FullNewSales { 
     pub sale: NewSale,
-    pub sales_products: Vec<FullNewSaleProduct>
+    pub sales_products: Vec<NewProductSaleInfo>
 }
 
