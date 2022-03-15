02fe3b25dd0461ae38d023d426fcb07004c82172
philipdaquin <philipdaquin20@gmail.com>
2022-03-15 14:17:24 +0800

    Sales Service

--- inventory/src/models/mod.rs
+++ inventory/src/models/mod.rs
@@ -2,3 +2,4 @@ pub mod product;
 pub mod prices;
 pub mod errors;
 pub mod sales;
+pub mod product_sales; 
--- inventory/src/models/sales.rs
+++ inventory/src/models/sales.rs
@@ -5,3 +5,35 @@ use diesel::{RunQueryDsl, QueryDsl};
 use crate::schema::{self, sales, sale_products};
 use crate::types::{ProductColumns, PRODUCT_COLUMNS};
 use juniper::FieldResult;
+use chrono::NaiveTime;
+
+
+
+
+
+pub struct Sales { 
+    pub id: i32, 
+    pub user_id: i32,
+    pub sale_date: NaiveTime,
+    pub total: f64,
+    pub bill_number: Option<String>
+}
+
+pub struct NewSales { 
+    pub id: Option<i32>, 
+    pub user_id: Option<i32>,
+    pub sale_date: Option<NaiveTime>,
+    pub total: Option<f64>,
+    pub bill_number: Option<String>
+}
+
+pub struct FullSale { 
+    pub sale: Sales,
+    pub sales_products: Vec<FullNewSales>
+}
+
+pub struct FullNewSales { 
+    pub sale: NewSales,
+    pub sales_products: Vec<FullNewSaleProduct>
+}
+
